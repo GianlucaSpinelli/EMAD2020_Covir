7709928ee41e53a9271e1b0ed4807e7c659b9538
GianlucaSpinelli <gianluca.spinelli97@hotmail.it>
2020-10-30 13:20:39 +0100

    commit Gianluca

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Capini workingon your app!</Text>
+      <Text> ciao workingon your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
