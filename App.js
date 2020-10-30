2d8a8bc89565af9ccac603d241333ebacdcd2244
enzop9898 <vincenzopecoraro_@libero.it>
2020-10-30 13:16:06 +0100

    ultimo

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Capgemini workingon your app!</Text>
+      <Text> Capini workingon your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
