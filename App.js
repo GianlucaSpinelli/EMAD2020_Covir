bd4ff9975a788103a905ef9f32946e01f28ac05f
Davide D'Andrea <ddandrea998@gmail.com>
2020-11-04 15:57:23 +0100

    primo commit davide

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text> Covir Covir Covir </Text>
+      <Text> Michele Covir Covir Covir </Text>
       <StatusBar style="auto" />
     </View>
   );
